74c7cac73a4bd17df69dc3142f9b7e784dce119c
krapivkin <119895881+krapivkin@users.noreply.github.com>
2023-01-07 18:40:01 +0300

    conflict confirm
    add code

--- 3-2/Program.cs
+++ 3-2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Таблица кубов от 1 до Вашего числа: "); 
+for (int i = 1; i <= number; i++)
+{
+        Console.Write ($"{i * i * i} ");
+}
